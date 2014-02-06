58700c1c6e389e8730d55cf1baa2f3623036e2ad
Oren Shoham <oshoham@oberlin.edu>
2014-02-06 10:03:57 -0500

    Added skeleton of logic for lexical analysis. Still have to handle error checking, comments, and ensure that Scanner exits gracefully on error.

--- Scanner.py
+++ Scanner.py
@@ -51,3 +51,117 @@ class Token:
 class Scanner:
     
     def __init__(self, file_name):
+        self.input_file = open(file_name)
+        self.current_line = self.input_file.readline()
+        self.line_number = 1
+        self.next_token = None
+
+    def get_next_token(self):
+        i = 0
+        while(i < len(self.current_line) and current_line[i].isspace()):
+            i += 1
+        if(i == len(self.current_line)):
+            self.current_line = self.input_file.readline()
+            if not current_line:
+                self.next_token = Token('T_EOF', "", self.line_number)
+            else:
+                self.line_number += 1
+                self.get_next_token()
+        else: # i < len(self.current_line)
+            if(self.current_line[i].isdigit()):
+                j = i+1
+                while(j < len(self.current_line) and self.current_line[j].isdigit()):
+                    j += 1
+                token_string = self.current_line[i:j]
+                self.next_token = Token('T_NUM', token_string, self.line_number)
+                self.current_line = self.current_line[j:]
+            elif(self.current_line[i].isalpha()):
+                j = i+1
+                while(j < len(self.current_line) and self.current_line[j].isalnum()):
+                    j += 1
+                token_string = self.current_line[i:j]
+                self.next_token = Token('T_ID', token_string, self.line_number)
+                self.current_line = self.current_line[j:]
+            elif(self.current_line[i] == '+'):
+                self.next_token = Token('T_PLUS', "+", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '-'):
+                self.next_token = Token('T_MINUS', "-", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == ';'):
+                self.next_token = Token('T_SEMICOLON', ";", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == ','):
+                self.next_token = Token('T_COMMA', ",", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '['):
+                self.next_token = Token('T_LBRACKET', "[", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == ']'):
+                self.next_token = Token('T_RBRACKET', "]", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '{'):
+                self.next_token = Token('T_LBRACE', "{", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '}'):
+                self.next_token = Token('T_RBRACE', "}", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '('):
+                self.next_token = Token('T_LPAREN', "(", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == ')'):
+                self.next_token = Token('T_RPAREN', ")", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '<'):
+                j = i+1
+                if(self.current_line[j] == '='):
+                    self.next_token = Token('T_LEQ', "<=", self.line_number)
+                    self.current_line = self.current_line[j+1:]
+                else:
+                    self.next_token = Token('T_LESS', "<", self.line_number)
+                    self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '>'):
+                j = i+1
+                if(self.current_line[j] == '='):
+                    self.next_token = Token('T_GEQ', ">=", self.line_number)
+                    self.current_line = self.current_line[j+1:]
+                else:
+                    self.next_token = Token('T_GREATER', ">", self.line_number)
+                    self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '='):
+                j = i+1
+                if(self.current_line[j] == '='):
+                    self.next_token = Token('T_EQ', "==", self.line_number)
+                    self.current_line = self.current_line[j+1:]
+                else: # TODO: exit gracefully
+                    pass
+            elif(self.current_line[i] == '!'):
+                j = i+1
+                if(self.current_line[j] == '='):
+                    self.next_token = Token('T_NEQ', "!=", self.line_number)
+                    self.current_line = self.current_line[j+1:]
+                else: # TODO: exit gracefully
+                    pass
+            elif(self.current_line[i] == '*'):
+                j = i+1
+                if(self.current_line[j] == '/'):
+                    pass # TODO: handle error where we see end of comments identifier without start of comments
+                else:
+                    self.next_token = Token('T_MULT', "*", self.line_number)
+                    self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '/'):
+                j = i+1
+                if(self.current_line[j] == '*'):
+                    pass # TODO: handle comments
+                else:
+                    self.next_token = Token('T_DIV', "/", self.line_number)
+                    self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '%'):
+                self.next_token = Token('T_MOD', "%", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            elif(self.current_line[i] == '&'):
+                self.next_token = Token('T_AND', "&", self.line_number)
+                self.current_line = self.current_line[i+1:]
+            else:
+                print("ERROR: Unidentifiable Token!")
+                pass # TODO: handle error where we don't find any tokens
