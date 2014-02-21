6b157fee89c2176600c3b7d0e31c3b6878744474
Oren Shoham <oshoham@oberlin.edu>
2014-02-21 11:15:12 -0500

    Started changing the file structure to make packages behave nicely.

---
+++ bpl/Parser.py
@@ -0,0 +1,72 @@
+"""
+AUTHOR: Oren Shoham
+DATE: Feb. 16, 2014
+
+A Parser for the BPL programming language. Implemented for CS331 at Oberlin College.
+"""
+
+from Scanner import *
+
+NodeType = enum('DECLARATION',
+        'STATEMENT',
+        'EXPRESSION')
+
+class TreeNode(object):
+    def __init__(self, kind, line_number):
+        self.kind = getattr(NodeType, kind)
+        self.line_number = line_number
+
+class DecNode(TreeNode):
+    def __init__(self, kind, line_number, next_dec)
+        TreeNode.__init__(self, kind, line_number)
+        self.next_dec = next_dec
+
+class VarDecNode(DecNode):
+    def __init__(self, kind, line_number, next_dec, token_type, is_pointer = False):
+        DecNode.__init__(self, kind, line_number, next_dec)
+        self.token_type = token_type
+        self.is_pointer = is_pointer
+
+class FunDecNode(DecNode):
+    def __init__(self, kind, line_number, next_dec, return_type, params, body):
+        DecNode.__init__(self, kind, line_number, next_dec)
+        self.return_type = return_type
+        self.params = params
+        self.body = body
+
+class ArrayDecNode(VarDecNode):
+    def __init__(self, kind, line_number, next_dec, token_type, is_pointer = False, size):
+        VarDecNode.__init__(self, kind, line_number, next_dec, token_type, is_pointer)
+        self.size = size
+
+class StatementNode(TreeNode):
+    def __init__(self, kind, line_number, next_statement):
+        TreeNode.__init__(self, kind, line_number)
+        self.next_statement = next_statement
+
+class ExpressionStatement(StatementNode):
+    def __init__(self, kind, line_number, next_statement):
+        StatementNode.__init__(self, kind, line_number, next_statement)
+
+class ParserException(Exception):
+    pass
+
+class Parser:
+
+    def __init__(self, input_file):
+        self.scanner = Scanner(input_file)
+        self.scanner.next_token = scanner.get_next_token()
+
+    def declaration_list(self):
+        d = declaration()
+        head = d
+        while self.scanner.next_token.kind != TokenType.T_EOF:
+            d1 = declaration()
+            d.next_dec = d1
+            d = d1
+        return head
+
+    def declaration(self):
+        if not is_type_token(self.scanner.next_token):
+            raise ParserException()
+        type_token = self.scanner.next_token
--- bpl/scanner/scanner.py
+++ bpl/scanner/scanner.py
@@ -1,69 +1,17 @@
+"""
+AUTHOR: Oren Shoham
+DATE: Feb. 16, 2014
+
+A lexical scanner for the BPL programming language. Implemented for CS331 at Oberlin College.
+"""
+
 import sys
-
-# Hacking together an Enum data type
-def enum(*sequential, **named):
-    enums = dict(zip(sequential, range(len(sequential))), **named)
-    return type('Enum', (), enums)
-
-# An "Enum" that maps Token types onto integers.
-# TokenType.T_ID => 0, TokenType.T_ID => 1, etc.
-TokenType = enum('T_ID',
-        'T_NUM',
-        'T_INT',
-        'T_VOID',
-        'T_STRING',
-        'T_IF',
-        'T_ELSE',
-        'T_WHILE',
-        'T_RETURN',
-        'T_WRITE',
-        'T_WRITELN',
-        'T_READ',
-        'T_SEMICOLON',
-        'T_COMMA',
-        'T_LBRACKET',
-        'T_RBRACKET', 
-        'T_LBRACE', 
-        'T_RBRACE',
-        'T_LPAREN',
-        'T_RPAREN',
-        'T_LESS',
-        'T_LEQ',
-        'T_EQ',
-        'T_NEQ',
-        'T_GEQ',
-        'T_GREATER',
-        'T_ASSIGN',
-        'T_PLUS',
-        'T_MINUS',
-        'T_MULT',
-        'T_DIV',
-        'T_MOD',
-        'T_AND',
-        'T_STRVAL',
-        'T_EOF')
-
-# A Token is the atomic unit of BPL. Each Token keeps track of its kind, 
-# the actual string it represents, and the line number on which it occurs.
-class Token:
-
-    # kind should be a string that is a valid TokenType (see above)
-    # value should be a string
-    # line_number should be an integer
-    def __init__(self, kind, value, line_number):
-        self.kind = getattr(TokenType, kind)
-        self.value = value
-        self.line_number = line_number
-    
-    # Return a string representation that displays the Token's
-    # kind, value, and line number.
-    def __str__(self):
-        return "Token " + str(self.kind) + ", string " + self.value + ", line number " + str(self.line_number)
+from Token import *
 
 # The Scanner performs a lexical analysis of a BPL file. It keeps track of
 # its current position in the file, and uses the variable next_token and the method 
 # get_next_token() to step through the file, one Token at a time.
-class Scanner:
+class Scanner(object):
 
     # Constructor for the Scanner. Takes a file object as the only parameter,
     # sets the current line to the first line of said file, initalizes next_token
