4813695677fc3da633b18bf4750e95214edd0b59
Oren Shoham <oshoham@oberlin.edu>
2014-03-01 15:21:32 -0500

    Added declaration function.

--- bpl/parser/parser.py
+++ bpl/parser/parser.py
@@ -8,7 +8,9 @@ A Parser for the BPL programming language. Implemented for CS331 at Oberlin Coll
 from bpl.scanner.scanner import Scanner
 from bpl.scanner.token import TokenType, Token, is_type_token
 
-NodeType = enum('DECLARATION',
+NodeType = enum('VAR_DEC',
+        'FUN_DEC',
+        'ARRAY_DEC',
         'STATEMENT',
         'EXPRESSION')
 
@@ -19,7 +21,7 @@ class TreeNode(object):
         self.next_node = next_node
 
 class DecNode(TreeNode):
-    def __init__(self, kind, line_number, name, type_token, next_node = None)
+    def __init__(self, kind, line_number, name, type_token, next_node = None):
         TreeNode.__init__(self, kind, line_number, next_node)
         self.name = name
         self.type_token = type_token
@@ -71,9 +73,11 @@ class Parser(object):
     def expect(self, token, message):
         current_token = self.scanner.next_token
         if current_token.kind != getattr(TokenType, token):
-            raise ParserException(message)
+            raise ParserException('Parser error on line {}: {}'.format(
+                self.scanner.line_number,
+                message))
         self.scanner.get_next_token()
-
+        return current_token
 
     def declaration_list(self):
         d = declaration()
@@ -84,7 +88,44 @@ class Parser(object):
             d = d1
         return head
 
-    def declaration(self):
+    def declaration(self, local=False):
+        is_pointer = False
+        line_number = self.scanner.line_number
         if not is_type_token(self.scanner.next_token):
-            raise ParserException()
+            raise ParserException('Error on line {}: Expected a type token to begin a declaration.'.format(line_number))
         type_token = self.scanner.next_token
+        self.scanner.get_next_token()
+
+        if self.scanner.next_token.kind == TokenType.T_MULT:
+            is_pointer = True
+            self.scanner.get_next_token()
+        
+        id_token = self.expect('T_ID', 'Expected an identifier as part of the declaration.')
+
+        # handle variable declarations
+        if self.scanner.next_token.kind == TokenType.T_SEMICOLON:
+           self.scanner.get_next_token() 
+           return VarDecNode('VAR_DEC', line_number, id_token.value, type_token, is_pointer)
+
+        # handle array declarations
+        elif self.scanner.next_token.kind == TokenType.T_LBRACKET:
+            self.scanner.get_next_token()
+            size = int(self.expect('T_NUM', 'Expected an integer size as part of the array declaration.').value)
+            self.expect('T_RBRACKET', 'Expected a right bracket as part of the array declaration.')
+            self.expect('T_SEMICOLON', 'Expected a semicolon to end the array declaration.')
+            return ArrayDecNode('ARRAY_DEC', line_number, id_token.value, type_token, size, is_pointer)
+
+        # handle function delcarations
+        elif self.scanner.next_token.kind == TokenType.T_LPAREN:
+            if is_pointer:
+                raise ParserException('Error on line {}: Cannot declare a pointer to a function.'.format(line_number))
+            if local:
+                raise ParserException('Error on line {}: Cannot declare a function inside a compound statement.'.format(line_number))
+            self.scanner.get_next_token()
+            parameters = self.params()
+            self.expect('T_RPAREN', 'Expected a right parenthesis to end the function parameter declarations.')
+            body = self.compound_statement()
+            return FunDecNode('FUN_DEC', line_number, id_token.value, type_token, params, body)
+
+        else:
+            raise ParserException('Error on line {}: Unexpected token in declaration.'.format(line_number))
