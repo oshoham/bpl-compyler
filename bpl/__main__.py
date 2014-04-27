dd834bab651f14ceb949f62ae7dc39207fb4cdad
Oren Shoham <oshoham@oberlin.edu>
2014-04-27 15:37:11 -0400

    Rewrote compilation script as a Python executable.

--- bpl/__main__.py
+++ bpl/__main__.py
@@ -5,13 +5,14 @@ from bpl.type_checker.type_checker import TypeCheckerException, type_check
 import sys, os
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 3:
         file_name = sys.argv[1]
+        output_file_name = sys.argv[2]
     else:
-        print "Error: No file name supplied."
+        print "Error: Not enough arguments supplied to bpl main module."
         sys.exit()
     if not file_name.endswith('.bpl'):
-        print "Error: File is not a .bpl file!"
+        print "Error: File does not have a .bpl extension!"
         sys.exit()
     try:
         input_file = open(file_name)
@@ -29,8 +30,8 @@ if __name__ == "__main__":
     except TypeCheckerException as t:
         print t.message
         sys.exit()
-    output_file_name = os.path.basename(file_name).rstrip('.bpl') + '.s'
-    output_file = open(os.path.join(os.path.dirname(file_name), output_file_name), 'w')
+    #output_file_name = os.path.basename(file_name).rstrip('.bpl') + '.s'
+    output_file = open(output_file_name, 'w')
     generate_code(parse_tree, output_file)
     input_file.close()
     output_file.close()
