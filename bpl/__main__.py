7f469d2b61b379b2a99dd31af962499151c5ec62
Oren Shoham <oshoham@oberlin.edu>
2014-04-30 10:47:08 -0400

    Added exit status testing to bplc script.

--- bpl/__main__.py
+++ bpl/__main__.py
@@ -10,26 +10,26 @@ if __name__ == "__main__":
         output_file_name = sys.argv[2]
     else:
         print "Error: Not enough arguments supplied to bpl main module."
-        sys.exit()
+        sys.exit(1)
     if not file_name.endswith('.bpl'):
         print "Error: File name does not have a .bpl extension!"
-        sys.exit()
+        sys.exit(1)
     try:
         input_file = open(file_name)
     except IOError:
         print "Error: File not found!"
-        sys.exit()
+        sys.exit(1)
     try: 
         parser = Parser(input_file)
         parse_tree = parser.parse()
     except (ScannerException, ParserException) as e:
         print e.message
-        sys.exit()
+        sys.exit(1)
     try:
         type_check(parse_tree)
     except TypeCheckerException as t:
         print t.message
-        sys.exit()
+        sys.exit(1)
     #output_file_name = os.path.basename(file_name).rstrip('.bpl') + '.s'
     output_file = open(output_file_name, 'w')
     generate_code(parse_tree, output_file)
